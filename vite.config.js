7e7a93b05785aa80d53b412d07311852f5e0228c
eloy6lega <eloy.perez@campusfp.es>
2024-02-01 15:00:26 +0100

    CambiosDef

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+// import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [sveltekit(), svelte()],
-	test: {
-		globals: true,
-	  },
+	plugins: [sveltekit()],
+	// test: {
+	// 	globals: true,
+	//   },
 	server: {
 		proxy: {
 			// IP de casa
